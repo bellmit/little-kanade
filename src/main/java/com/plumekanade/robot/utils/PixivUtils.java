4bfc9ecf7bdfb8905654bff08188a9f76a159ea8
plumekanade <kanade@plumekanade.com>
2021-12-10 11:59:09 +0800

    添加图片压缩依赖

--- src/main/java/com/plumekanade/robot/utils/PixivUtils.java
+++ src/main/java/com/plumekanade/robot/utils/PixivUtils.java
@@ -5,15 +5,18 @@ import com.plumekanade.robot.constants.ProjectConst;
 import com.plumekanade.robot.vo.PixivArtwork;
 import com.plumekanade.robot.vo.ResultMsg;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 import static com.plumekanade.robot.constants.PixivConst.*;
@@ -65,6 +68,22 @@ public class PixivUtils {
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  /**
+   * 图片压缩
+   */
+  public void compressImg(InputStream in, String path) throws IOException {
+
+    File file = new File(path);
+    if (!file.getParentFile().exists()) { // 创建目录
+      // noinspection ResultOfMethodCallIgnored
+      file.getParentFile().mkdirs();
+    }
+    Thumbnails.of(in).size(2560, 1440)
+        // .keepAspectRatio(false)   // 是否遵循原图比例 false不遵循
+        // .scale(1f)        // 缩放
+        .outputQuality(0.9f).toFile(file);
   }
 
 }
